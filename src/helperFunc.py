f083ce795e30256d32c0d5c62a8ddc55703ed42c
Mor <moroknin@gmail.com>
2019-12-11 09:48:17 +0200

    Add files via upload

---
+++ src/helperFunc.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+
+def getToken( url,login,password ):
+  endpoint = url
+  data = {"username":login, "password":password}
+  headers = {'Content-Type' : 'application/x-www-form-urlencoded' }
+  p = requests.post(endpoint, data,headers=headers, verify=False)
+	
+  if p.status_code == 201:
+    return p.text
+  else:
+    return p
+
+def getCluster( url, token ):
+  headers = {'authorization': "Bearer " + token }
+  response = requests.get(url, headers=headers, verify=False)
+  #logger.debug( response.text)
+  jData = json.loads(response.text)
+  return jData['cluster']['nodes']
+
+def convertStatus(status):
+  if status == 'CONNECTED':
+    return 1
+  else:
+    return 0
+
+def getFlow( url, token ):
+  headers = {'authorization': "Bearer " + token }
+  response = requests.get(url, headers=headers, verify=False)
+  jData = json.loads(response.text)
+  return jData['controllerStatus']
+
+def about(url , token):
+  headers = {'authorization': "Bearer " + token }
+  response = requests.get(url, headers=headers, verify=False)
+  jData = json.loads(response.text)
+  return jData['about']
+
+def getProcessorFlow(url,token):
+  headers = {'authorization': "Bearer " + token }
+  response = requests.get(url, headers=headers, verify=False)
+  jData = json.loads(response.text)
+  return jData['processGroups']
